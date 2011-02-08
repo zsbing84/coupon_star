24654b5720ca10c9ac949dc453a5d9c4c69dee7b
zsbing84 <zsbing84@hotmail.com>
2011-02-09 00:25:36 +0900

    test

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'http://rubygems.org'
+
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'spree', '0.40.2'
+gem 'mysql', '2.8.1'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CouponStar::Application.config.secret_token = '4a78698bcd59735350c6856f92d921af0f3b279ad470b924bc92f808fd36c0367af4514a14e563ff5ceb0c5fa83c305ab35c980804035f788f39d4bd9f177573'
